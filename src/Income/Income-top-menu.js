7a9ebd772a6df451dcdb35b86cb8ed208fe80f04
daniel <alphdanm@gmail.com>
2020-07-03 08:16:13 -0500

    ajuste de menus

--- src/Income/Income-top-menu.js
+++ src/Income/Income-top-menu.js
@@ -3,16 +3,19 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+
+
 export default class IncomeTopMenu extends React.Component{
     render(){
         return(
-        <AppBar position = "static">
+        <AppBar position = "static"
+                style={{ backgroundColor: '#FFFFFF', color : '#000000', fontWeight: '-moz-initial' }}>
           <Tabs 
             centered
             variant = "fullWidth">
-            <Tab label ="Nuevo"></Tab>
-            <Tab label ="Lista"></Tab>
-            <Tab label ="Resumen"></Tab>
+            <Tab label ="Nuevo" style ={{fontWeight : "bold"}}></Tab>
+            <Tab label ="Lista" style ={{fontWeight : "bold"}}></Tab>
+            <Tab label ="Resumen" style ={{fontWeight : "bold"}}></Tab>
           </Tabs>
         </AppBar>
         )
